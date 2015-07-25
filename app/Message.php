453679015d70e342226eeb1f68f537c282c7ecdc
benjamin sweetnam <ben_s247@optusnet.com.au>
2015-07-25 22:29:54 +1000

    Article, Email, Images, Message and User, Migrations and Models created

---
+++ app/Message.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+            protected $fillable = [
+                                    'sender',
+                                    'email',
+                                    'subject',
+                                    'message'];
+}
